94d789d50cf50a778916470bd1be9071c7db2e25
simone-dotolo <simonedotolo@libero.it>
2023-05-03 22:00:57 +0200

    docstring and fix Q bug

--- metrics.py
+++ metrics.py
@@ -3,6 +3,19 @@ from math import floor
 from scipy import ndimage as ndi
 
 def SAM(predictions, labels):
+    '''
+        Spectral Angle Mapper (SAM).
+
+        Args:
+            predictions:  Numpy Array
+                The Fused image. Dimensions: H, W, Bands
+            labels : Numpy Array
+                The reference image. Dimensions: H, W, Bands
+
+        Returns:
+            angle : float
+                The SAM index in degree.
+    '''
     
     preds_norm = np.sum(predictions ** 2, axis=-1)
     labels_norm = np.sum(labels ** 2, axis=-1)
@@ -12,14 +25,28 @@ def SAM(predictions, labels):
     product_norm[product_norm == 0] = np.nan
     preds_labels_prod = preds_labels_prod.flatten()
     product_norm = product_norm.flatten()
-    # TODO: maybe replace np.sum() / product_norm.shape[0] with np.mean()
-    angle = np.sum(np.arccos(np.clip(preds_labels_prod / product_norm, a_min=-1, a_max=1)), axis=-1) / product_norm.shape[0]
+    angle = np.mean(np.arccos(np.clip(preds_labels_prod / product_norm, a_min=-1, a_max=1)))
     angle = angle * 180 / np.pi
     return angle
 
 
 def ERGAS(predictions, labels, ratio):
-    
+    '''
+        Erreur Relative Globale Adimensionnelle de Synthèse (ERGAS).
+        
+        Args:
+            predictions : Numpy Array
+                The Fused image. Dimensions: H, W, Bands
+            labels : Numpy Array
+                The reference image. Dimensions: H, W, Bands
+            ratio : int
+                PAN-MS resolution ratio
+
+        Returns:
+            ergas_index : float
+                The ERGAS index.
+    '''
+
     nbands = labels.shape[-1]
     mu = np.mean(labels, axis=(0, 1)) ** 2
     error = np.mean((predictions - labels) ** 2, axis=(0, 1))
@@ -27,6 +54,21 @@ def ERGAS(predictions, labels, ratio):
     return ergas
 
 def Q(predictions, labels, block_size=32):
+    '''
+        Universal Image Quality Index (UIQI).
+
+        Args:
+            predictions : Numpy Array
+                The Fused image. Dimensions: H, W, Bands
+            labels : Numpy Array
+                The reference image. Dimensions: H, W, Bands
+            block_size : int
+                The windows size on which calculate the Q index
+
+        Returns:
+            quality : float
+                The UIQI index.
+    '''
 
     N = block_size ** 2
     nbands = predictions.shape[-1]
@@ -47,8 +89,12 @@ def Q(predictions, labels, block_size=32):
         var1 = p_sq_sum / N - mu1 ** 2
         var2 = l_sq_sum / N - mu2 ** 2
         cov12 = p_l_sum / N - mu1 * mu2
-        indexes = ((var1 + var2) != 0) & ((mu1 ** 2 + mu2 ** 2) != 0)
         quality_map = np.ones(p_sum.shape)
+        indexes = ((var1 + var2) == 0) & ((mu1 ** 2 + mu2 ** 2) != 0)
+        quality_map[indexes] = (2 * mu1[indexes] * mu2[indexes]) / (mu1[indexes] ** 2 + mu2[indexes] ** 2)
+        indexes = ((var1 + var2) != 0) & ((mu1 ** 2 + mu2 ** 2) == 0)
+        quality_map[indexes] = (2 * cov12[indexes]) / (var1[indexes] + var2[indexes])
+        indexes = ((var1 + var2) != 0) & ((mu1 ** 2 + mu2 ** 2) != 0)
         quality_map[indexes] = (4 * cov12[indexes] * mu1[indexes] * mu2[indexes]) / (var1[indexes] + var2[indexes]) / (mu1[indexes] ** 2 + mu2[indexes] ** 2)
         q_indexes[i] = np.mean(quality_map, axis=(0,1))
     
